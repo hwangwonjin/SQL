0d3ac6c2ca4c1894624d7e8478696449ebcd53b6
wonjin <hdigimon27@gmail.com>
2022-08-25 13:09:17 +0900

    2022/08/25

--- 제 2장 테이블 제약조건과 수정.sql
+++ 제 2장 테이블 제약조건과 수정.sql
@@ -80,11 +80,16 @@ alter table `user5` add `hp` varchar(20);
 alter table `user5` add `age` char(10) default'0000-00-00' after `name`;
 alter table `user5` add `uid` varchar(10);
 
-
-
 #실습 2-12
+alter table `user5` modify `hp` char(13);
+alter table `user5` modify `age` tinyint;
+
 #실습 2-13
+alter table `user5` change column `addr` `address` varchar(100);
+
 #실습 2-14
+alter table `user5` drop `gender`;
+
 #실습 2-15
 create table `user6` like `user5`;
 
