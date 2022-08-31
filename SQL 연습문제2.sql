c3f387c2de833c3310fdccca0afd40945a7e1c50
wonjin <hdigimon27@gmail.com>
2022-08-31 11:36:01 +0900

    SQL 연습문제2.sql

--- SQL 연습문제2.sql
+++ SQL 연습문제2.sql
@@ -50,9 +50,9 @@ insert into `bank_account` values('101-11-1003','S1','자유저축예금','87083
 insert into `bank_account` values('101-11-2001','S1','자유저축예금','220-82-52237',23000000,'2003-04-14');
 insert into `bank_account` values('101-11-2002','S1','자유저축예금','361-22-42687',4201000,'2008-12-30');
 insert into `bank_account` values('101-11-2003','S1','자유저축예금','102-22-51094',8325010,'2010-06-07');
-insert into `bank_account` values('101-12-1002','S2','정기적금예금','830513-1000003',1020000,'2011-05-13');
+insert into `bank_account` values('101-12-1002','S2','정기적금예금','830513-2000003',1020000,'2011-05-13');
 insert into `bank_account` values('101-13-1005','S3','주택청약예금','941127-1000006',720800,'2012-10-15');
-insert into `bank_account` values('101-21-1004','L1','고객신용대출','910912-1000005',1200500,'2009-08-25');
+insert into `bank_account` values('101-21-1004','L1','고객신용대출','910912-2000005',1200500,'2009-08-25');
 insert into `bank_account` values('101-22-1006','L2','예금담보대출','730423-1000001',25000,'2013-12-11');
 insert into `bank_account` values('101-23-1007','L3','주택담보대출','750210-1000002',2700000,'2020-09-23');
 
@@ -87,7 +87,8 @@ select `a_item_dist`,`a_item_name` from `bank_account`;
 select distinct`a_item_dist`,`a_item_name` from `bank_account`;
 
 #실습2-10
-select * from `bank_account` where `a_item_name` = '자유저축예금' and `a_balance` >= 1000000;
+select * from `bank_account` 
+where `a_item_name` = '자유저축예금' and `a_balance` >= 1000000;
 
 #실습2-11
 select * from `bank_customer` where `c_addr` like '경기도%'; 
@@ -99,30 +100,112 @@ select * from `bank_customer` where `c_dist` like 2;
 select * from `bank_transaction` where `t_dist` like 1;
 
 #실습2-14
-select * from `bank_transaction` where `t_dist` like 2 order by `t_amount` desc;
+select * from `bank_transaction` where `t_dist` like 2 
+order by `t_amount` desc;
 
 #실습2-15
-select sum(`t_amount`) as `임금총합`, avg(`t_amount`) as `임금평균` from `bank_transaction` where `t_dist`=1;
+select sum(`t_amount`) as `임금총합`, avg(`t_amount`) as `임금평균` 
+from `bank_transaction` where `t_dist`=1;
 
 #실습2-16
-select max(`t_amount`) as `큰 금액`,min(`t_amount`) as `작은 금액` from `bank_transaction` where `t_dist` = 2;
+select max(`t_amount`) as `큰 금액`,min(`t_amount`) as `작은 금액` 
+from `bank_transaction` where `t_dist` = 2;
 
 #실습2-17
-select* from `bank_account` where `a_item_dist` = 'S1' order by `a_balance` desc;
+select* from `bank_account` where `a_item_dist` = 'S1' 
+order by `a_balance` desc;
 
 #실습2-18
+select * from `bank_account` where `a_item_dist` = 'S1'
+order by `a_balance`desc
+limit 1;
+
 #실습2-19
+select * from `bank_transaction` where `t_amount`
+order by `t_dist`, `t_amount` desc;
+
 #실습2-20
+select count(case when`t_dist`=1 then 1 end) as `입금 건수`, count(case when `t_dist`=2 then 2 end) as `출금 건수`,
+count(case when `t_dist`=3 then 3 end) as `조회 건수`
+from `bank_transaction`;
+
 #실습2-21
+select `t_dist`, case when `t_dist`= 1 then '입금'
+ when `t_dist` = 2 then '출금' when `t_dist` = 3 then '조회'
+ end as `type`,`t_a_no`,`t_amount` from `bank_transaction`;
+
 #실습2-22
+select `t_dist`, count(`t_no`) from `bank_transaction` group by `t_dist`;
+
 #실습2-23
+select `t_a_no`, `t_dist`, sum(`t_amount`) from `bank_transaction` 
+where `t_dist` = 1 group by `t_a_no`;
+
 #실습2-24
+select `t_a_no`,`t_dist`,sum(`t_amount`) as `합계` 
+from `bank_transaction` where `t_dist` = 1 group by `t_a_no`
+having `합계`>100000 order by `합계` desc ;
+
 #실습2-25
+select * from `bank_account`as a
+join `bank_customer` as c
+on a.a_c_no=c.c_no;
+
 #실습2-26
+select `a_no` as `계좌번호`,`a_item_name` as `계좌이름`,
+`a_c_no` as `주민번호`,`c_no` as `고객명`,`a_balance` as `현재잔액` 
+from `bank_account` as a
+join `bank_customer` as b
+on a.a_c_no=b.c_no;
+
 #실습2-27
+select * from `bank_transaction` as a
+join `bank_account` as b
+on a.t_a_no = b.a_no;
+
 #실습2-28
+select 
+		`t_no` as `거래번호`,
+        `t_a_no` as `계좌번호`,
+        `a_c_no` as `고객번호(주민번호)`,
+        `t_dist` as `거래구분`,
+        `t_amount` as `거래금액`,
+        `t_datetime` as `거래일자`
+from `bank_account` as a
+join `bank_transaction` as b
+on a.a_no = b.t_a_no;
+
 #실습2-29
+select 
+	`t_no`,
+    `a_no`,
+    `c_no`,
+    `t_dist`,
+    `a_item_name`,
+    `c_name`,
+    `t_amount`,
+    `t_datetime`
+ from `bank_transaction` as a
+join `bank_account` as b on a.t_a_no=b.a_no
+join `bank_customer`as c on b.a_c_no = c.c_no;
+
 #실습2-30
+select 
+	`t_no`,
+    `a_no`,
+    `c_no`,
+    `t_dist`,
+    `a_item_name`,
+    `c_name`,count(`t_no`) as `거래건수`
+ from `bank_account` as a
+join `bank_transaction` as b on a.a_no = b.t_a_no
+join`bank_customer` as c on a.a_c_no = c.c_no
+where `t_dist` in(1,2) and `c_dist` = 1
+group by `c_name`
+order by `t_dist`, `거래건수` desc;
+
+
+
 
 
 
